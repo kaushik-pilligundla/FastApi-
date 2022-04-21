4315f493ba0d3bef536b3bccaf7727cbfcc87e94
kaushik-pilligundla <79022525+kaushik-pilligundla@users.noreply.github.com>
2022-04-20 20:28:38 -0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,14 +2,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    database_hostname: str = "localhost"
-    database_port: str = "5432"
-    database_password: str = "NAMAKA"
-    database_name: str = "fastapi"
-    database_username: str = "postgres"
-    secret_key: str = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-    algorithm: str = "HS@%5"
-    access_token_expire_minutes: int = 60
+    database_hostname: str 
+    database_port: str 
+    database_password: str = 
+    database_name: str 
+    database_username: str
+    secret_key: str 
+    algorithm: str
+    access_token_expire_minutes: int
 
 
 settings = Settings()
